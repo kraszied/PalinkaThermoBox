c779606c0f80162f19f280d82d0f5d382958f291
Simon Velez <simfelduv@gmail.com>
2012-08-08 18:31:16 -0500

    > Added Support for small screens (second try) > Reorganized and refactored code in the drawing method.

--- Source/src/kb/apps/palinkathermobox/TemperatureControl.java
+++ Source/src/kb/apps/palinkathermobox/TemperatureControl.java
@@ -11,9 +11,7 @@ import android.view.View;
 
 public class TemperatureControl extends View {
 
-	public final static int METER_RADIUS = 15; 
-	public final static int METER_SLACK = 15;
-	
+
 	public final static int MINIMUM_TEMPERATURE = 8;
 	public final static int MAXIMUM_TEMPERATURE = 24; //TODO : Confirm values
 	public final static double PERCENTAGE_TO_ANGLE =  1.8;
@@ -21,8 +19,8 @@ public class TemperatureControl extends View {
 	public static final String PREFERENCE_NAME = "PALINCAR_REFERENCE";    /** prefernece name */
 	private static final String SAVED_PERCENTAGE_KEY = "PathPercentage";  /** stored temperature key */
   
-  private static final double NO_INPUT_TEMP = -99;                /** there is no input temperature */
-  private static final String NO_INPUT_TEMP_STRING = "-.-- °C";   /** until the device is not connected this value is shown */
+	private static final double NO_INPUT_TEMP = -99;                /** there is no input temperature */
+	private static final String NO_INPUT_TEMP_STRING = "-.-- °C";   /** until the device is not connected this value is shown */
 
 	private Paint paint;
 	private int pathPercentage; 
@@ -30,6 +28,9 @@ public class TemperatureControl extends View {
 	
 	private float meterXPosition;
 	private float meterYPosition;
+	private int height;
+	private int width;
+	private int referenceSize;
 	private boolean isMeterPressed;
 	private ApplicationEvents event;
 	private Context context;
@@ -40,10 +41,11 @@ public class TemperatureControl extends View {
 	{
 		super(context,atSet);
 		paint = new Paint();
-		pathPercentage = 75;
+		pathPercentage = 50;
 		meterXPosition = 0;
 		meterYPosition = 0;
 		isMeterPressed = false;
+		referenceSize = 160;
 		
     // load the stored temperature data
 		this.context = context;
@@ -63,39 +65,42 @@ public class TemperatureControl extends View {
 		super.onDraw(canvas);
 		paint.setAntiAlias(true);
 		
+		//Pre-calculate sizes
+		this.height = this.getHeight();
+		this.width = this.getWidth();
+		
 		//Support for small screens, re-scaling
-		if(this.getWidth() < 400)
+		if(this.width > 400 && referenceSize == 160 )
 		{
-			canvas.scale(0.75f,0.75f); 
+			referenceSize *= 0.7;
 		}
 		canvas.drawARGB(255, 20, 20, 20);
 		
 		//Main, big yellow circle.
 		paint.setARGB(255, 240, 230, 20);
+		canvas.drawCircle(this.width/2, this.height/2, referenceSize ,paint);
 		
-		canvas.drawCircle(this.getWidth()/2, this.getHeight()/2, 160,paint);
-		
-		//inner black circle
+		//inner black circle, at 0.8125 of reference size
 		paint.setARGB(255, 0, 0, 0);
-		canvas.drawCircle(this.getWidth()/2, this.getHeight()/2, 130,paint);
-		canvas.drawLine(this.getWidth()/2-160, this.getHeight()/2,this.getWidth()/2+160, this.getHeight()/2, paint);
-		canvas.drawLine(this.getWidth()/2, this.getHeight()/2,this.getWidth()/2, this.getHeight()/2 + 160, paint);
+		canvas.drawCircle(this.width/2, this.height/2, (float) (referenceSize*0.8125) ,paint);
+		canvas.drawLine(this.width/2 - referenceSize, this.height/2, this.width/2 + referenceSize, this.height/2, paint);
+		canvas.drawLine(this.width/2, this.height/2, this.width/2, this.height/2 + referenceSize, paint);
 		paint.setARGB(255, 240, 230, 20);
-		canvas.drawCircle(this.getWidth()/2, this.getHeight()/2, 126,paint);
+		//Inner yellow area, at 0.7875 of reference size
+		canvas.drawCircle(this.width/2, this.height/2, (float) (referenceSize*0.7875),paint);
+		
 		
 		blackControlDegrees = (pathPercentage * PERCENTAGE_TO_ANGLE) * Math.PI/180;
-		
-		
-		
 		paint.setTextAlign(Align.CENTER);
 		paint.setTextSize(45);
 		
 		//Draw small black circle
-		//depends on temeperatures
-		double meterXDifference = 145 * Math.cos(blackControlDegrees);
-		double meterYDifference = 145 * Math.sin(blackControlDegrees);
-		meterXPosition = (float) (this.getWidth()/2 + meterXDifference); 
-		meterYPosition = (float) (this.getHeight()/2 + meterYDifference);
+		//depends on temeperatures, radius is set at 0.90625 of reference size 
+		double meterXDifference = (referenceSize * 0.90625) * Math.cos(blackControlDegrees);
+		double meterYDifference = (referenceSize * 0.90625) * Math.sin(blackControlDegrees);
+		meterXPosition = (float) (this.width/2 + meterXDifference); 
+		meterYPosition = (float) (this.height/2 + meterYDifference);
+		// Set color of the meter, depending on user interaction
 		if(isMeterPressed)
 		{
 			paint.setARGB(255, 190, 40, 40);
@@ -105,7 +110,8 @@ public class TemperatureControl extends View {
 		{
 			paint.setARGB(255, 40, 40, 40);
 		}
-		canvas.drawCircle(meterXPosition, meterYPosition, METER_RADIUS, paint);
+		//Meter size is set at 0.09375 of reference size
+		canvas.drawCircle(meterXPosition, meterYPosition, (float) (referenceSize * 0.09375), paint);
 		
 		//Text-Drawing here
 		paint.setARGB(255, 40, 40, 40);
@@ -113,9 +119,10 @@ public class TemperatureControl extends View {
 		if (temperatureLevel == NO_INPUT_TEMP) {
 		  temperature = NO_INPUT_TEMP_STRING;
 		}
-		canvas.drawText(temperature, this.getWidth()/2, (this.getHeight()/2) + ((paint.getTextSize() / 2) - 4), paint);
+		canvas.drawText(temperature, this.width/2, (this.height/2) + ((paint.getTextSize() / 2) - 4), paint);
 		
 		//Meter values drawing
+		// Values are outside at 1.15625 of reference isze
 		paint.setARGB(255, 240, 230, 20);
 		paint.setTextSize(18);
 		int meterTemperature = MINIMUM_TEMPERATURE;
@@ -126,8 +133,8 @@ public class TemperatureControl extends View {
 			currenttemp = String.valueOf(meterTemperature);
 			currenttemp += "°C";
 			canvas.drawText(currenttemp, 
-							(float) (this.getWidth()/2 + 185 * Math.cos(textDegrees * Math.PI/180)), 
-							(float) ( (this.getHeight()/2 + 10) + 185 * Math.sin(textDegrees * Math.PI/180)), 
+							(float) (this.width/2 + (referenceSize * 1.15625) * Math.cos(textDegrees * Math.PI/180)), 
+							(float) ( (this.height/2 + 10) + (referenceSize * 1.15625) * Math.sin(textDegrees * Math.PI/180)), 
 							paint);
 			meterTemperature += 2;
 			textDegrees += 22.5;
@@ -140,11 +147,11 @@ public class TemperatureControl extends View {
 	public boolean onTouchEvent(MotionEvent event)
 	{
 		
-		
+		//Hit slack is set at X2 radius of meter ball
 		if(event.getAction() == MotionEvent.ACTION_DOWN)
 		{		
 			double hitRadius = Math.sqrt( Math.pow( (event.getX() - meterXPosition), 2) + Math.pow( (event.getY() - meterYPosition), 2) );
-			if(hitRadius < METER_RADIUS + METER_SLACK)
+			if(hitRadius < (referenceSize * 0.1875) )
 			{
 				isMeterPressed = true;
 				invalidate();
@@ -154,7 +161,7 @@ public class TemperatureControl extends View {
 		}
 		else if(event.getAction() == MotionEvent.ACTION_MOVE)
 		{
-			double angleDegrees = Math.atan2(event.getY() - this.getHeight()/2, event.getX() - this.getWidth()/2 );
+			double angleDegrees = Math.atan2(event.getY() - this.height/2, event.getX() - this.width/2 );
 			angleDegrees = Math.abs(angleDegrees) * 180/Math.PI;
 			pathPercentage = (int) (angleDegrees * (1/PERCENTAGE_TO_ANGLE));
 			invalidate();
